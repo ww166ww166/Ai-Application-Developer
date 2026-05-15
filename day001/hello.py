9012661e0d4357f482d8274642cfeca4c39d856b
ww166ww166 <136683429+ww166ww166@users.noreply.github.com>
2026-05-16 01:12:10 +0800

    Update hello.py

--- day001/hello.py
+++ day001/hello.py
@@ -1,4 +1,4 @@
 print("你好，Python")
-print("zheshiwodedyigepythonwenjian")
-print("qingduoduozhijiao")
-print("diyitianxuexi,yidingyaojianchidaozuihou")
+print("这是我的一个Python文件")
+print("请多多指教")
+print("第一天学习，一定要坚持到最后")
